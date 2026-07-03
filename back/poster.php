d08a6788394f2e0b4ae5a91aa2116d9efe65b91e
may <s6351301@gmail.com>
2026-07-03 16:01:30 +0800

    建立預告片清單版型

--- back/poster.php
+++ back/poster.php
@@ -1,6 +1,26 @@
 <div style="height:360px">
     <div class="ct">預告片清單</div>
+<div class='ct' style="display:flex;justify-content:space-between">
+    <div style="width:25%">預告片海報</div>
+    <div style="width:25%">預告片片名</div>
+    <div style="width:25%">預告片排序</div>
+    <div style="width:25%">操作</div>
+</div>
+<form action="./api/edit_poster.php" method="post">
+<div style="height:270px;overflow: auto;">
+    <div style="background:white;margin:3px 0;height:100px;display:flex;justify-content:space-between" >
+        <div style="width:25%">1</div>
+        <div style="width:25%">2</div>
+        <div style="width:25%">3</div>
+        <div style="width:25%">4</div>
+    </div>
 
+</div>
+<div class="ct">
+    <input type="submit" value="編輯確定">
+    <input type="reset" value="重置">
+</div>
+</form>
 </div>
 <hr>
 <div style="height:150px">
